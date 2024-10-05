8a14b0284b55ddf821f40dbe2c6ab775abd549f9
theryskin <theryskin@yandex.ru>
2024-10-05 22:06:15 +0300

    Update contact.php

--- site/contact.php
+++ site/contact.php
@@ -1,4 +1,28 @@
 <?php
-$title = "Контакты - Лабораторная работа № 3";
-include 'index.php';
+include 'header.html';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = htmlspecialchars($_POST['name']);
+    $message = htmlspecialchars($_POST['message']);
+    $category = $_POST['category'];
+    $attachment = isset($_POST['attachment']) ? htmlspecialchars($_POST['attachment']) : '';
+
+    echo "<p>Здравствуйте, $name!</p>";
+
+    if ($category == 'propose') {
+        echo "<p>Спасибо за ваше предложение:</p>";
+    } else {
+        echo "<p>Мы рассмотрим Вашу жалобу:</p>";
+    }
+
+    echo "<textarea readonly>$message</textarea>";
+
+    if ($attachment) {
+        echo "<p>Вы приложили следующий файл: $attachment</p>";
+    }
+
+    echo "<a class='btn' href='index.php?name=$name&email=" . urlencode($_POST['email']) . "&source=" . urlencode($_POST['source']) . "'>Заполнить снова</a>";
+} else {
+    echo "<p>Ошибка: данные формы не были отправлены.</p>";
+}
 ?>
